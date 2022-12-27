e4772519634dde3eecef3a0ffbfee93f68389233
ivanpascoe <sinh.rivera@gmail.com>
2022-12-27 12:02:59 -0600

    sintax fixed, solar flares

--- physics/lib/solartest.ex
+++ physics/lib/solartest.ex
@@ -3,7 +3,7 @@ defmodule Solartest do
   use Timex
 
   setup do
-    flares = [
+     flares = [
       %{calssification: :X, scale: 99, date: Date.from({1859,8,29})},
       %{calssification: :M, scale: 5.8, date: Date.from({2015,1,12})},
       %{calssification: :M, scale: 1.2, date: Date.from({2015,2,9})},
@@ -13,12 +13,19 @@ defmodule Solartest do
       %{calssification: :X, scale: 72, date: Date.from({2012,7,23})},
       %{calssification: :X, scale: 45, date: Date.from({2003,11,4})},
     ]
-    {:ok, data: flares}
+{:ok, data: flares}
   end
+
 
   test "We have 8 solar flares", %{data: flares} do
-    assert length(falres) == 8
-
+    assert length(flares) == 8
   end
-
+  test "Go inside", %{data: flares} do
+    d = Solar.no_eva(flares)
+    assert length(d) == 3
+  end
+  test "Deadliest", %{data: flares} do
+    d = Solar.deadliest(flares)
+    assert d == 99000
+  end
 end
