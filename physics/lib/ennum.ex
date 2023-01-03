0fb7b76311049bfdda315dbee4b9877186e1c846
ivanpascoe <sinh.rivera@gmail.com>
2023-01-03 14:12:39 -0600

    exploración de las funciones de Enum (random)

--- physics/lib/ennum.ex
+++ physics/lib/ennum.ex
@@ -1,14 +1,30 @@
 defmodule Ennum do
   def mapa, do: %{producto: :cheve, costo: 32}
-  def lista, do: [32, 34, 25, 15, 12]
-  def data, do: [:atom, "string", 45]
+  def lista, do: [a: 32, b: 34, c: 25, d: 15, e: 12]
+  def data, do: [:atom, "strIng", 45, a: :keyword]
 
   def try() do
-    mapa = [23, 23, 35, 40 , 12, 23, 56]
-    #  Enum.fetch(mapa,7)
-    # Enum.drop_every(mapa, 5)
-    # mapa = [23, 23, 35, 40 , 12, 23, 56]
-    # Enum.drop(mapa, 7)
+    dat = [35, 40 , 12, 23, 56]
+    Enum.random(lista())
+
+      # Enum.random(1..5000) - Entrega un numero al azar de la lista o dentro de un rango.
+      # Enum.product(dat)  -- Multiplica los elementos
+      # Enum.join(dat, " / ") - Une los elementos y permite especificar un separador
+      # Enum.intersperse(dat, 0) - intrduce eel caracter dado entre los elementos
+      # Enum.group_by(~w{ant buffalo cat dingo}, &String.length/1, &String.first/1)
+      # Enum.group_by(~w{ant buffalo cat dingo}, &String.length/1)
+      # Enum.frequencies_by(~w{aaa aA bbb cc c}, &String.length/1)
+      #  Enum.frequencies(ant buffalo ant ant buffalo dingo)
+      # dat = [35, 40 , 12, 23, 56]
+      # Enum.flat_map_reduce(dat, 0, fn x, acc ->{[[x]], acc + x} end)
+      # Enum.flat_map(dat, fn x -> [x, x] end)
+      #  Enum.map(lista(), fn {k,v} -> {k, v} end
+      #  Enum.find_value(mapa, fn x -> rem(x, 2) == 0 end)
+      # Enum.filter(mapa, fn x -> rem(x, 2) == 0 end)
+      #  Enum.fetch(mapa,7)
+      # Enum.drop_every(mapa, 5)
+      # mapa = [23, 23, 35, 40 , 12, 23, 56]
+      # Enum.drop(mapa, 7)
 
       # mapa = [23..30, 45..49, 12..15]
       # Enum.concat(mapa)
@@ -17,14 +33,15 @@ defmodule Ennum do
       # mapa = [[23, [12, 13], 15], [24, 345] ]
       # Enum.concat(mapa)
       # resultado [23, [12, 13], 15, 24, 345]
-    # Enum.chunk_by(mapa, (fn(x) -> rem(x, 2) ==1 end))
-    #  [[23], '\f', [13, 15]]
-    #  "stri\\\"ng"
-    # Enum.empty?(mapa)
-    # Enum.count(mapa)
-    # Enum.any?(mapa)
-    # Enum.all?(mapa, fn(x) -> x < 100 end)
-    # Enum.map(mapa, fn(x) -> x * 3.1 end)
-    #   |> Enum.max juganding
-  end
+      # Enum.chunk_by(mapa, (fn(x) -> rem(x, 2) ==1 end))
+      #  [[23], '\f', [13, 15]]
+      #  "stri\\\"ng"
+      # Enum.empty?(mapa)
+      # Enum.count(mapa)
+      # Enum.any?(mapa)
+      # Enum.all?(mapa, fn(x) -> x < 100 end)
+      # Enum.map(mapa, fn(x) -> x * 3.1 end)
+      #   |> Enum.max juganding
+    end
+    #  Enum.into(lista(), %{})
 end
